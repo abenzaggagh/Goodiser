9864211061853c651b287c3a6564a1ac91bd15a2
Amine <4min3b3n@gmail.com>
2019-04-12 16:58:02 +0100

    Remastering

---
+++ app/src/main/java/com/goodiser/android/app/ProductActivity.java
@@ -0,0 +1,16 @@
+package com.goodiser.android.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.goodiser.android.R;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+
+}
